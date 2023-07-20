c5caafd80465001c19bcedf3cf0184c6dfb919c9
Sepideh Alassi <>
2023-07-20 16:44:27 +0200

    add tests for fa and en language detections

--- tests/input_lang_test.py
+++ tests/input_lang_test.py
@@ -3,9 +3,21 @@ from pipes.preprocess_pipe import detect_lang
 
 
 class InputLanguageTest(unittest.TestCase):
-    def test_supported_lang(self):
+    def test_de_lang(self):
         lang = detect_lang("hallo meine Liebe, wie geht es dir?")
         self.assertEqual(lang, 'de')
+
+    def test_en_lang(self):
+        text = "The fleet left Spain on 20 September 1519, sailing west across the Atlantic toward South America. " \
+               "In December, they made landfall at Rio de Janeiro."
+        lang = detect_lang(text)
+        self.assertEqual(lang, 'en')
+
+    def test_fa_lang(self):
+        text = "سوئیس از جنوب با ایتالیا، از غرب با فرانسه، از شمال با آلمان و اتریش و از شرق با لیختناشتاین همسایه است. " \
+               "بزرگترین شهرها و مراکز اقتصادی سوئیس از جمله  ژنو و بازل در قسمت فلات آن واقع شده اند. برن پایتخت سوئیس است و قطب های اقتصادی آن ژنو و زوریخ هستند."
+        lang = detect_lang(text)
+        self.assertEqual(lang, 'fa')
 
     def test_unsupported_lang(self):
         text = "De voila baguette dans frenchtech manger un camembert putain. Comme même être carrément quand même vouloir putain du coup. Du vin."
